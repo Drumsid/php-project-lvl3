c054aa89b1f399cfdb2a240b64da6392986df9a8
densidnay@gmail.com <Jesuschrist05882>
2021-02-05 10:51:01 +0300

    start work to tests

--- tests/Feature/UrlControllerTest.php
+++ tests/Feature/UrlControllerTest.php
@@ -4,14 +4,20 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-use App\Models\Url;
+// use App\Models\Url;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class UrlControllerTest extends TestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
-        Url::factory()->count(2)->make();
+        DB::table('urls')->insert([
+            ['name' => 'exampleSite1.com', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'exampleSite2.ru', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'exampleSite3.com', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+        ]);
     }
     public function testIndex()
     {
