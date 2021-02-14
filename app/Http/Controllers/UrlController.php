1f7b7411f5a0c5676efee53e3a7e8325e688f643
Denis <den-sidnay@yandex.ru>
2021-02-14 11:26:33 +0300

    fix migrations

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -15,14 +15,14 @@ class UrlController extends Controller
     public function index()
     {
         $urls = DB::table('urls')->orderBy('id', 'asc')->get();
-        $checks = DB::table('url_checks')->orderBy('url_id', 'asc')->orderBy('created_at', 'desc')->distinct('url_id')->get();
+        $checks = DB::table('urls_checks')->orderBy('url_id', 'asc')->orderBy('created_at', 'desc')->distinct('url_id')->get();
         $lastCheck = $checks->keyBy('url_id');
         return view('urls.index', compact('urls', 'lastCheck'));
     }
     public function show($id)
     {
         $url = DB::table('urls')->find($id);
-        $cheks = DB::table('url_checks')->where('url_id', '=', $id)->get();
+        $cheks = DB::table('urls_checks')->where('url_id', '=', $id)->get();
         return view('urls.show', compact('url', 'cheks'));
     }
     public function store(Request $request)
@@ -70,7 +70,7 @@ class UrlController extends Controller
         $keywords = optional($document->first('meta[name=keywords]'))->getAttribute('content');
         $description = optional($document->first('meta[name=description]'))->getAttribute('content');
         if ($response->ok()) {
-            DB::table('url_checks')->insert([
+            DB::table('urls_checks')->insert([
                 'url_id' => $id,
                 'status_code' => $response->status(),
                 'h1' => $h1,
@@ -99,7 +99,7 @@ class UrlController extends Controller
     public function checkDestroy($id) // only for dev
     {
         if ($id) {
-            DB::table('url_checks')->where('id', '=', $id)->delete();
+            DB::table('urls_checks')->where('id', '=', $id)->delete();
             flash('Данные о проверке удалены!')->success();
         }
         return back();
--- database/migrations/2021_02_04_185912_create_urls_checks_table.php
+++ database/migrations/2021_02_04_185912_create_urls_checks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUrlChecksTable extends Migration
+class CreateUrlsChecksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUrlChecksTable extends Migration
      */
     public function up()
     {
-        Schema::create('url_checks', function (Blueprint $table) {
+        Schema::create('urls_checks', function (Blueprint $table) {
             $table->id();
             $table->integer('url_id');
             $table->integer('status_code')->default(200);
@@ -31,6 +31,6 @@ class CreateUrlChecksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('url_checks');
+        Schema::dropIfExists('urls_checks');
     }
 }
