5e024c9dc480e2c742445448c81a8c8d29d1c4f1
Denis <den-sidnay@yandex.ru>
2021-02-18 21:38:11 +0300

    more fixes urlCheks

--- app/Http/Controllers/UrlChecksController.php
+++ app/Http/Controllers/UrlChecksController.php
@@ -17,28 +17,28 @@ class UrlChecksController extends Controller
         $url = DB::table('urls')->find($id);
         try {
             $response = Http::get($url->name);
-            $document = new Document($response->body());
-            $h1 = optional($document->first('h1'))->text();
-            $keywords = optional($document->first('meta[name=keywords]'))->getAttribute('content');
-            $description = optional($document->first('meta[name=description]'))->getAttribute('content');
-            if ($response->ok()) {
-                DB::table('url_checks')->insert([
-                    'url_id' => $id,
-                    'status_code' => $response->status(),
-                    'h1' => $h1,
-                    'keywords' => $keywords,
-                    'description' => $description,
-                    'created_at' => $url->created_at,
-                    'updated_at' => Carbon::now()
-                ]);
-                DB::table('urls')->where('id', $id)->update(
-                    ['updated_at' => Carbon::now()]
-                );
-                flash('Сайт проанализирован!')->warning();
-                return redirect()->route('urls.show', $id);
-            }
         } catch (RequestException | ConnectionException $e) {
             flash("Error: {$e->getMessage()}")->error();
+            return redirect()->route('urls.show', $id);
+        }
+        $document = new Document($response->body());
+        $h1 = optional($document->first('h1'))->text();
+        $keywords = optional($document->first('meta[name=keywords]'))->getAttribute('content');
+        $description = optional($document->first('meta[name=description]'))->getAttribute('content');
+        if ($response->ok()) {
+            DB::table('url_checks')->insert([
+                'url_id' => $id,
+                'status_code' => $response->status(),
+                'h1' => $h1,
+                'keywords' => $keywords,
+                'description' => $description,
+                'created_at' => $url->created_at,
+                'updated_at' => Carbon::now()
+            ]);
+            DB::table('urls')->where('id', $id)->update(
+                ['updated_at' => Carbon::now()]
+            );
+            flash('Сайт проанализирован!')->warning();
             return redirect()->route('urls.show', $id);
         }
     }
