1fbabc3cf722720cc9820cf9f0b4a2d370384e50
Denis <den-sidnay@yandex.ru>
2021-02-19 21:11:16 +0300

    fixed

--- app/Http/Controllers/UrlChecksController.php
+++ app/Http/Controllers/UrlChecksController.php
@@ -15,28 +15,27 @@ class UrlChecksController extends Controller
     public function store(int $id): object
     {
         $url = DB::table('urls')->find($id);
+        abort_unless($url, 404);
         try {
             $response = Http::get($url->name);
             $document = new Document($response->body());
             $h1 = optional($document->first('h1'))->text();
             $keywords = optional($document->first('meta[name=keywords]'))->getAttribute('content');
             $description = optional($document->first('meta[name=description]'))->getAttribute('content');
-            if ($response->ok()) {
-                DB::table('url_checks')->insert([
-                    'url_id' => $id,
-                    'status_code' => $response->status(),
-                    'h1' => $h1,
-                    'keywords' => $keywords,
-                    'description' => $description,
-                    'created_at' => $url->created_at,
-                    'updated_at' => Carbon::now()
-                ]);
-                DB::table('urls')->where('id', $id)->update(
-                    ['updated_at' => Carbon::now()]
-                );
-                flash('Сайт проанализирован!')->warning();
-                return redirect()->route('urls.show', $id);
-            }
+            DB::table('url_checks')->insert([
+                'url_id' => $id,
+                'status_code' => $response->status(),
+                'h1' => $h1,
+                'keywords' => $keywords,
+                'description' => $description,
+                'created_at' => $url->created_at,
+                'updated_at' => Carbon::now()
+            ]);
+            DB::table('urls')->where('id', $id)->update(
+                ['updated_at' => Carbon::now()]
+            );
+            flash('Сайт проанализирован!')->warning();
+            return redirect()->route('urls.show', $id);
         } catch (RequestException | ConnectionException $e) {
             flash("Error: {$e->getMessage()}")->error();
         }
